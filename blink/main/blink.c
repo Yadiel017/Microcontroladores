53099bfc178293e4c8062acda38728b5396417c7
Yadiel <aquino200517@gmail.com>
2026-01-22 13:45:04 -0400

    Añadidos del build

--- blink/main/blink.c
+++ blink/main/blink.c
@@ -1,6 +1,6 @@
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <driver/gpio.h:
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <stdio.h>
 
 #define LED_GPIO 2
